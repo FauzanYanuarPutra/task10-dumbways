861d3bea087c7f041ff2c4c2e99646f7157d00d4
Fauzanyan <ccdplayerx@gmail.com>
2023-08-09 18:01:25 +0700

    responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,3 +61,21 @@ function showAdd() {
   idForm.classList.toggle("hidden");
   bg.classList.toggle("hidden");
 }
+
+// function menuSidebar() {
+//   const sidebar = document.querySelector(".sidebar");
+//   sidebar.classList.toggle("hidden");
+// }
+
+function menuSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  const closeButton = sidebar.querySelector(".close-button");
+
+  sidebar.classList.toggle("hidden");
+
+  if (sidebar.classList.contains("hidden")) {
+    closeButton.style.left = "-60px"; // Hide the close button
+  } else {
+    closeButton.style.left = "0"; // Show the close button
+  }
+}
