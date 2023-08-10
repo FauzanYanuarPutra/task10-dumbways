963e56ae53d2e25b68533a24d4018d1edd062acf
Fauzanyan <ccdplayerx@gmail.com>
2023-08-10 19:32:46 +0700

    perbaikan preview image

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,3 +64,121 @@ function showAdd() {
   bg.classList.toggle("hidden");
   body.classList.toggle("no-scroll");
 }
+
+// POST BLOG / PROJECT
+
+const DataProject = [];
+
+function iconCard(technologies) {
+  console.log(technologies);
+
+  return technologies
+    .map(
+      (tech) =>
+        `<img src="./assets/svg/${tech}.svg" alt="${tech}" style="width: 20px; height: 20px;">`
+    )
+    .join("");
+}
+
+function renderProjects() {
+  const contentContainer = document.getElementById("content-card-project");
+  contentContainer.innerHTML = "";
+
+  DataProject.forEach((project) => {
+    const projectHTML = `
+          <a href="projectDetail.html" class="card-project">
+            <div>
+              <img src="${project.image}" alt="" style="width: 100%;">
+            </div>
+            <div>
+              <p class="judul">${project.name}</p>
+              <p>durasi : ${project.durasi} bulan</p>
+              <p class="about">${project.description}</p>
+              <div class="icon-project">
+                ${iconCard(project.technologies)}
+              </div>
+              <div class="button-project">
+                <div>Edit</div>
+                <div>Delete</div>
+              </div>
+            </div>
+          </a>
+        `;
+
+    contentContainer.innerHTML += projectHTML;
+  });
+}
+function addBlog(event) {
+  event.preventDefault();
+  console.log("Hallo");
+
+  const projectName = document.getElementById("project-name").value;
+  const startDate = document.getElementById("start-date").value;
+  const endDate = document.getElementById("end-date").value;
+  const description = document.getElementById("description").value;
+
+  const technologies = Array.from(
+    document.querySelectorAll(".tech-check input[type='checkbox']:checked")
+  ).map((checkbox) => checkbox.getAttribute("data-tech"));
+  console.log(technologies);
+
+  const imageInput = document.querySelector("#image");
+  const selectedImage = imageInput.files[0];
+  const imageUrl = selectedImage ? URL.createObjectURL(selectedImage) : null;
+
+  const durasi = calculateMonthDifference(startDate, endDate);
+  const error = document.querySelector(".checkbox-error-validate");
+  if (technologies.length === 0) {
+    error.classList.remove("hidden");
+    return;
+  } else {
+    error.classList.add("hidden");
+  }
+
+  const newProject = {
+    name: projectName,
+    startDate: startDate,
+    endDate: endDate,
+    description: description,
+    technologies: technologies,
+    image: imageUrl,
+    durasi: durasi,
+  };
+
+  console.log("New Project:", newProject);
+  DataProject.push(newProject);
+  console.log(DataProject);
+  renderProjects();
+}
+
+function calculateMonthDifference(startDate, endDate) {
+  var start = new Date(startDate);
+  var end = new Date(endDate);
+
+  var yearDiff = end.getFullYear() - start.getFullYear();
+  var monthDiff = end.getMonth() - start.getMonth();
+
+  return yearDiff * 12 + monthDiff || 0;
+}
+
+// function previewImage(props) {
+//   const previewElement = document.querySelector("#preview-image");
+//   console.log(props);
+//   previewElement.textContent = `<img src="${props}">`;
+// }
+
+const imageInput = document.querySelector("#image");
+const imagePreview = document.querySelector("#image-preview");
+
+imageInput.addEventListener("change", function (event) {
+  const selectedImage = event.target.files[0];
+  const imageUrl = selectedImage ? URL.createObjectURL(selectedImage) : "";
+
+  imagePreview.src = imageUrl;
+
+  if (imageUrl) {
+    imagePreview.classList.remove("hidden");
+  } else {
+    imagePreview.classList.add("hidden");
+  }
+});
