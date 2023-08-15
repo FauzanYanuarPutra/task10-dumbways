3b3e50e426db1ccbaa75e44461d35e58d4e0b318
Fauzanyan <ccdplayerx@gmail.com>
2023-08-15 12:04:00 +0700

    no scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 const navLinks = document.getElementById("menu-container");
-
+const body = document.querySelector("body");
 const buttonMenu = () => {
   navLinks.classList.toggle("active");
+  body.classList.toggle("no-scroll");
 };
